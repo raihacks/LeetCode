efa03cd0d78940911f5a92931ec8b53da5550c45
raihacks <siddhirai326@gmail.com>
2026-01-02 09:09:26 +0530

    1

---
+++ JAN/Main27.java
@@ -0,0 +1,24 @@
+package JAN;
+
+public class Main27{
+    public static void main(String[] args){
+        Solution s = new Solution();
+        int[] nums = {3,2,2,3};
+        int val = 3;
+        int k = s.removeElement(nums, val);
+        System.out.println(k);
+    }
+}
+
+class Solution{
+    public int removeElement(int[] nums, int val){
+        int k = 0;
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] != val){
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
